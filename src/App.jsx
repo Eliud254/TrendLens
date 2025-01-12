ecacf84c03841b2636364611b0291ecef6bc67c9
Eliud254 <eliudobure@gmail.com>
2025-01-12 16:59:03 +0300

    UI rendering corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import './index.css'
 
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage'; // Import LandingPage component
 
+const App = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        {/* Define a route for the landing page */}
+        <Route path="/" element={<LandingPage />} /> 
+        {/* Add other routes as needed */}
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+// Add console.log to check if App component renders
+console.log("App component rendered"); 
+
+export default App;
